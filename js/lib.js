dd51a2129f7f00b0c533bba66e600b057a15777f
liulixingyun <liulixingyun@gmail.com>
2016-06-27 21:50:01 +0800

    Javascript files initial

---
+++ js/lib.js
@@ -0,0 +1,57 @@
+/**
+ * 通用工具库
+ */
+var lib = (function(){
+  "use strict";
+
+  /**
+   * 私有的方法
+   */
+  var private = (function() {
+
+  })();
+
+  /**
+   * DOM处理子模块库
+   */
+  var dom = (function(){
+
+  })();
+
+  /**
+   * 样式处理子模块库
+   */
+  var css = (function(){
+
+  })();
+
+  /**
+   * 事件处理子模块库
+   */
+  var event = (function(){
+
+  })();
+
+  /**
+   * ajax处理子模块库
+   */
+  var ajax = (function(){
+
+  })();
+
+  /**
+   * cookie处理子模块库
+   */
+  var cookie = (function(){
+
+  })();
+
+
+  return {
+    dom : dom,
+    css : css,
+    on : event,
+    ajax : ajax,
+    cookie : cookie
+  };
+})();
